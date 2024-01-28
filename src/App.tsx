58cb9fbe6944090e72f09a87ce8908b6bf1a63e8
Jan Kierejsza <jan.kierejsza@gmail.com>
2024-01-28 14:40:01 +0100

    Add required pages and routing mechanism

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import Episodes from "./pages/Episodes/Episodes";
+import Characters from "./pages/Characters/Characters";
+import CharacterDetails from "./pages/CharacterDetails/CharacterDetails";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to="/episodes" />} />
+        <Route path="/episodes" element={<Episodes />} />
+        <Route path="/characters" element={<Characters />} />
+        <Route path="/character-detials" element={<CharacterDetails />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
