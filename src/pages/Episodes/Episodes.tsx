a0b931d6c76433ec7b2591c75961e97c1f146d39
Jan Kierejsza <jan.kierejsza@gmail.com>
2024-01-29 23:51:31 +0100

    Add episodes page left side elements

--- src/pages/Episodes/Episodes.tsx
+++ src/pages/Episodes/Episodes.tsx
@@ -1,5 +1,27 @@
+import "./Episodes.css";
+import title_img from "../../assets/image.png";
+
 const Episodes = () => {
-  return <div>Episodes</div>;
+  return (
+    <div className="episodes">
+      <div className="episodes-left">
+        <div className="episodes-title">
+          <span className="text text-400 episodes-title-text-black">
+            Episodes of the <span className="text-700">4th</span>
+            <br></br>
+            season of the series
+          </span>
+          <span className="text episodes-title-text-cyan text-700">
+            Rick and Morty
+          </span>
+        </div>
+        <div className="episodes-img">
+          <img src={title_img} alt="Rick and Morty show image" />
+        </div>
+      </div>
+      <div className="episodes-right"></div>
+    </div>
+  );
 };
 
 export default Episodes;
