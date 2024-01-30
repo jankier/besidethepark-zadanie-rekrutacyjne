649d0fba01eec4f08af385e2bfbbc36f80bfe561
Jan Kierejsza <jan.kierejsza@gmail.com>
2024-01-30 14:22:16 +0100

    Add episodes page episodes list

--- src/pages/Episodes/Episodes.tsx
+++ src/pages/Episodes/Episodes.tsx
@@ -1,7 +1,34 @@
 import "./Episodes.css";
 import title_img from "../../assets/image.png";
+import { useEffect, useState } from "react";
 
 const Episodes = () => {
+  interface SeriesData {
+    id: number;
+    name: string;
+    air_date: string;
+    episode: string;
+    characters: string[];
+    url: string;
+    created: string;
+  }
+
+  const [seriesData, getSeriesData] = useState<undefined[] | SeriesData[]>([]);
+
+  useEffect(() => {
+    const authParameters = {
+      method: "GET",
+    };
+    fetch(
+      "https://rickandmortyapi.com/api/episode/?episode=S04",
+      authParameters
+    )
+      .then((result) => result.json())
+      .then((data) => getSeriesData(data.results));
+  }, []);
+
+  console.log(seriesData);
+
   return (
     <div className="episodes">
       <div className="episodes-left">
@@ -19,7 +46,22 @@ const Episodes = () => {
           <img src={title_img} alt="Rick and Morty show image" />
         </div>
       </div>
-      <div className="episodes-right"></div>
+      <div className="episodes-right">
+        {seriesData.map((episode, index) => {
+          if (episode === undefined) {
+            console.log(episode);
+          }
+          return (
+            <div key={index} className="episodes-episode text text-700">
+              <div className="episodes-no">{episode?.episode}</div>
+              <div className="episodes-info">
+                <div className="episodes-name">{episode?.name}</div>
+                <div className="episodes-date">{episode?.air_date}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
