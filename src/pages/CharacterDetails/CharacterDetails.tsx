0b8163912cda0dc092b305e3f220e2130bb50f57
Jan Kierejsza <jan.kierejsza@gmail.com>
2024-02-01 21:42:11 +0100

    Add characters details page left side elements

--- src/pages/CharacterDetails/CharacterDetails.tsx
+++ src/pages/CharacterDetails/CharacterDetails.tsx
@@ -1,5 +1,24 @@
+import Button from "../../components/Button/Button";
+import "./CharacterDetails.css";
+
 const CharacterDetails = () => {
-  return <div>CharacterDetails</div>;
+  return (
+    <section className="character-details">
+      <div className="character-details-left">
+        <div className="character-details-button">
+          <Button text="Characters" />
+        </div>
+        <div className="character-details-title">
+          <span className="text text-700 character-details-title-text-cyan">
+            Rick Sanchez
+          </span>
+        </div>
+        <div className="character-details-img">
+          <span className="text text-700">Character image</span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default CharacterDetails;
