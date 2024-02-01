cf6ab28f11e55ef2907268b17d9a55126735f150
Jan Kierejsza <jan.kierejsza@gmail.com>
2024-02-01 23:57:30 +0100

    Add characters details page list

--- src/pages/CharacterDetails/CharacterDetails.tsx
+++ src/pages/CharacterDetails/CharacterDetails.tsx
@@ -1,6 +1,45 @@
+import { useEffect, useState } from "react";
+import { useURLID } from "../../hooks/useURLID";
 import "./CharacterDetails.css";
 
+type CharacterData = {
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: {
+    name: string;
+  };
+  location: {
+    name: string;
+  };
+  image: string;
+};
+
 const CharacterDetails = () => {
+  const { id } = useURLID();
+
+  const [characterData, setCharacterData] = useState<
+    undefined | CharacterData
+  >();
+
+  useEffect(() => {
+    async function fetchCharacterData() {
+      try {
+        const character_data_res = await fetch(
+          `https://rickandmortyapi.com/api/character/${id}`
+        ).then((character_data_res) => character_data_res.json());
+        setCharacterData(character_data_res);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchCharacterData();
+  }, [id]);
+
+  console.log(characterData);
+
   return (
     <section className="character-details">
       <div className="character-details-left">
@@ -19,11 +58,43 @@ const CharacterDetails = () => {
         </button>
         <div className="character-details-title">
           <span className="text text-700 character-details-title-text-cyan">
-            Rick Sanchez
+            {characterData?.name}
           </span>
         </div>
         <div className="character-details-img">
-          <span className="text text-700">Character image</span>
+          <img src={characterData?.image} alt="image of character" />
+        </div>
+      </div>
+      <div className="character-details-right">
+        <div className="text text-700 character-details-info">
+          <div className="character-status">
+            <span>{characterData?.status}</span>
+            <span>Status</span>
+          </div>
+          <div className="character-species">
+            <span>{characterData?.species}</span>
+            <span>Species</span>
+          </div>
+          <div className="character-type">
+            {characterData?.type === "" ? (
+              <span>-</span>
+            ) : (
+              <span>{characterData?.type}</span>
+            )}
+            <span>Type</span>
+          </div>
+          <div className="character-gender">
+            <span>{characterData?.gender}</span>
+            <span>Gender</span>
+          </div>
+          <div className="character-origin">
+            <span>{characterData?.origin.name}</span>
+            <span>Origin</span>
+          </div>
+          <div className="character-location">
+            <span>{characterData?.location.name}</span>
+            <span>Last known location</span>
+          </div>
         </div>
       </div>
     </section>
