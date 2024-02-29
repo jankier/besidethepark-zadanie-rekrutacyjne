0240ef6446af279d9c7a77aec7b684306e4f7d92
Jan Kierejsza <jan.kierejsza@gmail.com>
2024-02-29 16:48:42 +0100

    Adjusted and added new tests for character details page

--- src/pages/CharacterDetails/Tests/CharacterDetails.test.tsx
+++ src/pages/CharacterDetails/Tests/CharacterDetails.test.tsx
@@ -1,93 +1,116 @@
 import "@testing-library/jest-dom/vitest";
-import { describe, it, expect, beforeAll, afterEach, afterAll } from "vitest";
+import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { http, HttpResponse } from "msw";
-import { setupServer } from "msw/node";
 import { BrowserRouter } from "react-router-dom";
+import { GET_CHARACTER_DATA } from "../../../GraphQL/Queries";
+import { MockedProvider } from "@apollo/client/testing";
+import { GraphQLError } from "graphql";
+import { ErrorBoundary } from "react-error-boundary";
+import FetchingError from "../../../components/FetchingError/FetchingError.tsx";
 import CharacterDetails from "../CharacterDetails.tsx";
 
-type PathParams = {
-  postId: string;
-};
-
-type CharacterData = {
-  name: string;
-  status: string;
-  species: string;
-  type: string;
-  gender: string;
-  origin: {
-    name: string;
-  };
-  location: {
-    name: string;
-  };
-  image: string;
-};
-
-const server = setupServer();
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
-
 describe("CharacterDetails", () => {
-  it("routing of the app", async () => {
-    const character_data = createCharacterData(
-      "Rick Sanchez",
-      "Alive",
-      "Human",
-      "-",
-      "Male",
-      { name: "Earth (C-137)" },
-      { name: "Citadel of Ricks" },
+  const characterDetailsMock = {
+    delay: 30,
+    request: {
+      query: GET_CHARACTER_DATA,
+    },
+    variableMatcher: () => true,
+    result: {
+      data: {
+        character: {
+          gender: "Male",
+          image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+          location: {
+            name: "Citadel of Ricks",
+          },
+          name: "Rick Sanchez",
+          origin: {
+            name: "Earth (C-137)",
+          },
+          species: "Human",
+          status: "Alive",
+          type: "",
+        },
+      },
+    },
+  };
+  it("render loading screen and character details page", async () => {
+    render(
+      <MockedProvider mocks={[characterDetailsMock]} addTypename={false}>
+        <ErrorBoundary FallbackComponent={FetchingError}>
+          <CharacterDetails />
+        </ErrorBoundary>
+      </MockedProvider>,
+      { wrapper: BrowserRouter }
+    );
+    expect(screen.getByTestId("loader-character-details-id"));
+    expect(await screen.findByText("Rick Sanchez"));
+    expect(await screen.findByText("Alive"));
+    expect(await screen.findByText("-"));
+    expect(await screen.findByText("Human"));
+    expect(await screen.findByText("Citadel of Ricks"));
+    expect(await screen.findByText("Earth (C-137)"));
+    const image = screen.getByAltText("image of character");
+    expect(image).toHaveAttribute(
+      "src",
       "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
     );
-    server.use(createGetCharacterData(character_data));
-    render(<CharacterDetails />, { wrapper: BrowserRouter });
-    expect(await screen.findByText(character_data.name));
-    const image = screen.getByAltText("image of character");
-    console.log(image);
-    expect(image).toHaveAttribute("src", character_data.image);
-    expect(await screen.findByText(character_data.status));
-    expect(await screen.findByText(character_data.species));
-    expect(await screen.findByText(character_data.type));
-    expect(await screen.findByText(character_data.origin.name));
-    expect(await screen.findByText(character_data.location.name));
+
+    screen.debug();
+  });
+
+  const characterDetailsNetworkErrorMock = {
+    delay: 30,
+    request: {
+      query: GET_CHARACTER_DATA,
+    },
+    variableMatcher: () => true,
+    error: new Error("Network error occured"),
+  };
+  it("simulate network error", async () => {
+    render(
+      <MockedProvider
+        mocks={[characterDetailsNetworkErrorMock]}
+        addTypename={false}
+      >
+        <ErrorBoundary FallbackComponent={FetchingError}>
+          <CharacterDetails />
+        </ErrorBoundary>
+      </MockedProvider>,
+      { wrapper: BrowserRouter }
+    );
+    expect(screen.getByTestId("loader-character-details-id"));
+    expect(await screen.findByText("Network error occured"));
+
+    screen.debug();
+  });
+
+  const characterDetailsGraphQLErrorMock = {
+    delay: 30,
+    request: {
+      query: GET_CHARACTER_DATA,
+    },
+    variableMatcher: () => true,
+    result: {
+      errors: [new GraphQLError("GraphQL error occured")],
+    },
+  };
+  it("simulate GraphQL error", async () => {
+    render(
+      <MockedProvider
+        mocks={[characterDetailsGraphQLErrorMock]}
+        addTypename={false}
+      >
+        <ErrorBoundary FallbackComponent={FetchingError}>
+          <CharacterDetails />
+        </ErrorBoundary>
+      </MockedProvider>,
+      { wrapper: BrowserRouter }
+    );
+    expect(screen.getByTestId("loader-character-details-id"));
+    expect(await screen.findByText("GraphQL error occured"));
 
     screen.debug();
   });
 });
-
-const createCharacterData = (
-  name: string,
-  status: string,
-  species: string,
-  type: string,
-  gender: string,
-  origin: { name: string },
-  location: { name: string },
-  image: string
-) => {
-  return {
-    name,
-    status,
-    species,
-    type,
-    gender,
-    origin,
-    location,
-    image,
-  };
-};
-
-const createGetCharacterData = (character_data: CharacterData) => {
-  return http.get<
-    PathParams,
-    never,
-    CharacterData,
-    "https://rickandmortyapi.com/api/character/:postId"
-  >("https://rickandmortyapi.com/api/character/:postId", async () => {
-    return HttpResponse.json(character_data);
-  });
-};
